1bd9823b62178b3de399955b87534cf48082c8d5
Ifthikar20 <ifthicaralikhan@gmail.com>
2025-02-01 21:33:25 -0600

    changes made to test UI nothing significant

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-
+"use client"
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
-   <div><h2>
-    this is just the landing apge</h2></div>
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <h2>This is just the landing page</h2>
+      <p className="text-2xl font-bold">{count}</p>
+      <Button variant="destructive" onClick={() => setCount(count + 1)}>
+        Click here
+      </Button>
+    </div>
   );
 }
